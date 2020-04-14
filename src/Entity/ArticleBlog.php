c57e52d184188ce9c1e20dbb2ec03d190c153686
Eva Saintier <eva.saintier@my-digital-school.org>
2020-04-14 11:52:14 +0200

    Ajout table "article_blog"

---
+++ src/Entity/ArticleBlog.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ArticleBlogRepository")
+ */
+class ArticleBlog
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Titre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Description_courte;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $Date;
+
+    /**
+     * @ORM\Column(type="string", length=3000)
+     */
+    private $Contenu;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $Auteur;
+
+    /**
+     * @ORM\Column(type="string", length=350, nullable=true)
+     */
+    private $Image;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->Titre;
+    }
+
+    public function setTitre(string $Titre): self
+    {
+        $this->Titre = $Titre;
+
+        return $this;
+    }
+
+    public function getDescriptionCourte(): ?string
+    {
+        return $this->Description_courte;
+    }
+
+    public function setDescriptionCourte(string $Description_courte): self
+    {
+        $this->Description_courte = $Description_courte;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->Date;
+    }
+
+    public function setDate(\DateTimeInterface $Date): self
+    {
+        $this->Date = $Date;
+
+        return $this;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->Contenu;
+    }
+
+    public function setContenu(string $Contenu): self
+    {
+        $this->Contenu = $Contenu;
+
+        return $this;
+    }
+
+    public function getAuteur(): ?string
+    {
+        return $this->Auteur;
+    }
+
+    public function setAuteur(string $Auteur): self
+    {
+        $this->Auteur = $Auteur;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->Image;
+    }
+
+    public function setImage(?string $Image): self
+    {
+        $this->Image = $Image;
+
+        return $this;
+    }
+}
